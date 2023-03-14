8a7466290922cd8ea4e6a08cb416a16ab14ed4bf
ugniussle <sleugnius@gmail.com>
2023-03-14 17:53:24 +0200

    create FileInput component

---
+++ resources/js/Components/FileInput.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['setFile']);
+
+const input = ref(null);
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <input
+        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        @input="$emit('setFile', $event.target.files[0])"
+        ref="input"
+        type="file"
+    />
+</template>
