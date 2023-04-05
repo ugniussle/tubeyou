f7dacb8c3ffec056ba2bbcdb570b608e8e5d4f59
ugniussle <sleugnius@gmail.com>
2023-04-05 16:05:12 +0300

    remove unused import

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\PlaylistController;
