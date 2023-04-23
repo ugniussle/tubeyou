7d11a5cfea24c20fb21ee5b97a97502d9d07b5bd
ugniussle <sleugnius@gmail.com>
2023-04-23 22:29:44 +0300

    player pausing and playing

--- resources/js/Components/VideoPlayer.vue
+++ resources/js/Components/VideoPlayer.vue
@@ -1,3 +1,49 @@
 <script setup>
-const props = defineProps();
+import { onMounted, onBeforeUnmount} from 'vue';
+const props = defineProps(['video']);
+
+const togglePlay = (event) => {
+        console.log(event)
+        let video = event.target
+
+        if(video.paused) {
+            video.play()
+        } else {
+            video.pause()
+        }
+}
+
+/**
+* @param {HTMLMediaElement} video
+*/
+const setupVideo = (video) => {
+    video.addEventListener("click", togglePlay)
+}
+
+/**
+* @param {HTMLMediaElement} video
+*/
+const unsetupVideo = (video) => {
+    video.removeEventListener("click", togglePlay)
+}
+
+onMounted(() => {
+    const video = document.getElementById("video")
+
+    setupVideo(video)
+})
+
+onBeforeUnmount(() => {
+    console.log("unmounting")
+    const video = document.getElementById("video")
+
+    unsetupVideo(video)
+})
 </script>
+
+<template>
+    <video id="video" class="w-full m-2 ml-0 aspect-video bg-black" width="320" height="240">
+        <source :src="video.video_asset">
+        Your browser does not support the video tag.
+    </video>
+</template>
